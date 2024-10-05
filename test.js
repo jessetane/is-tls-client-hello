087df887fb125d7d24008d673906429251cfbe43
Jesse Tane <jesse.tane@gmail.com>
2024-10-05 04:25:23 -0400

    swap tape for tap-esm

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-import tape from 'tape'
+import tap from 'tap-esm'
 import net from 'net'
 import http from 'http'
 import https from 'https'
 import isTLSClientHello from './index.js'
 
-tape('http', t => {
+tap('http', t => {
   t.plan(1)
 
   var tcpServer = net.createServer(socket => {
@@ -25,7 +25,7 @@ tape('http', t => {
   })
 })
 
-tape('https', t => {
+tap('https', t => {
   t.plan(1)
 
   var tcpServer = net.createServer(socket => {
@@ -46,7 +46,7 @@ tape('https', t => {
   })
 })
 
-tape('returns the number of bytes needed to parse the record if the buffer is not long enough', t => {
+tap('returns the number of bytes needed to parse the record if the buffer is not long enough', t => {
   t.plan(1)
   var length = 2
   var data = new Uint8Array([22, 3, 1, 0, length, 1])
